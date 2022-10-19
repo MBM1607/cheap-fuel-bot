672f6ac0e99962fdb09f3ee4cd4643eea95679f0
mbm_1607 <muhammadkhan1607@gmail.com>
2022-10-19 14:51:59 +0500

    Update url with heroku url

--- src/credentials.py
+++ src/credentials.py
@@ -1,3 +1,3 @@
 bot_token = "5768621673:AAFt41dqarGhnzgCV3rHQF-nJz1nBSXPh4k"
 bot_user_name = "projectzerothree"
-URL = "the heroku app link that we will create later"
+URL = "https://pz3-bot.herokuapp.com/"
