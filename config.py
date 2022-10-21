735faee0cadb769c2a74bd6d38383c58ad88f68e
mbm_1607 <muhammadkhan1607@gmail.com>
2022-10-21 11:28:55 +0200

    Update bot name & welcome message

--- config.py
+++ config.py
@@ -9,9 +9,8 @@ BOT_USERNAME = os.environ["BOT_USERNAME"]
 BOT_URL = os.environ["BOT_URL"]
 
 WELCOME_MESSAGE = """
-Welcome to PZ3 bot, the bot is using the service from https://projectzerothree.info/api.
-html to fetch the best prices for fuel, Please enter one of the regional options below to
-fetch the prices.
+Welcome to CheepFuel bot, this bot provides the service to fetch the best prices for fuel,
+Please enter one of the regional options below to fetch the prices.
 
 1. All
 2. VIC
