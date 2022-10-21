8bf45b00cdf1ed32eaa9ea4d8a7d4273c9aba92d
mbm_1607 <muhammadkhan1607@gmail.com>
2022-10-21 11:45:10 +0200

    Add error handling for index overflow

--- utils.py
+++ utils.py
@@ -6,38 +6,41 @@ import config
 
 
 def fetch_prices(text: str) -> str:
-    text = config.REGIONS[int(text) - 1] if text.isdigit() else text.upper()
+    try:
+        text = config.REGIONS[int(text) - 1] if text.isdigit() else text.upper()
 
-    if text not in config.REGIONS:
-        return "There was a problem in the name you used, please enter different name"
+        if text not in config.REGIONS:
+            return "There was a problem in the specied option. Check /help for instructions."
 
-    response = requests.get(config.API_URL)
+        response = requests.get(config.API_URL)
 
-    if response.ok:
-        json = response.json()
-        regional_prices = [
-            item["prices"] for item in json["regions"] if item["region"].upper() == text
-        ][0]
+        if response.ok:
+            json = response.json()
+            regional_prices = [
+                item["prices"]
+                for item in json["regions"]
+                if item["region"].upper() == text
+            ][0]
 
-        prices = [f"Lowest Prices for {text}"]
+            prices = [f"Lowest Prices for {text}"]
 
-        for price in regional_prices:
-            prices.append("")
-            prices.append(f"Type: {price['type']}")
-            prices.append(f"<b>Price: {price['price']}</b>")
-            prices.append(f"Name: {price['name']}")
-            prices.append(f"Suburb: {price['suburb']}")
+            for price in regional_prices:
+                prices.append("")
+                prices.append(f"Type: {price['type']}")
+                prices.append(f"<b>Price: {price['price']}</b>")
+                prices.append(f"Name: {price['name']}")
+                prices.append(f"Suburb: {price['suburb']}")
+                prices.append(
+                    f"Location: https://maps.google.com/?q={price['lat']},{price['lng']}"
+                )
+                prices.append("")
+
             prices.append(
-                f"Location: https://maps.google.com/?q={price['lat']},{price['lng']}"
+                f"Last Updated at: {datetime.fromtimestamp(json['updated']).isoformat(sep=' ')}"
             )
-            prices.append("")
 
-        prices.append(
-            f"Last Updated at: {datetime.fromtimestamp(json['updated']).isoformat(sep=' ')}"
-        )
-
-        return "\n".join(prices)
-    else:
-        return (
-            "There was a problem with fetching the prices from api. Please try later!"
-        )
+            return "\n".join(prices)
+        else:
+            return "There was a problem with fetching the prices from api. Please try later!"
+    except IndexError:
+        return "You chose an invalid option. Check /help for instructions."
